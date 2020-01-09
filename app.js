055513329b016af55f25f3041b5a7d6064b66350
jaggi47 <mr.ranveer7@gmail.com>
2020-01-09 20:01:11 +0530

    beginning

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const router = require('./router')
+
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use('/api', router);
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000, function(err,res) {
+    console.log(`Express server listening,`)
+})
